446a92025abed166e23d7614f4b81c5a51bf9abf
mkyong <mkyong2002@gmail.com>
2021-06-23 22:00:32 +0800

    add readme

---
+++ jersey/jersey-json-moxy/README.md
@@ -0,0 +1,3 @@
+# Jersey and JSON examples (EclipseLink MOXy)
+
+https://mkyong.com/webservices/jax-rs/jersey-and-json-examples-eclipselink-moxy/
--- jersey/jersey-json-moxy/src/main/java/com/mkyong/json/JsonResource.java
+++ jersey/jersey-json-moxy/src/main/java/com/mkyong/json/JsonResource.java
@@ -1,6 +1,11 @@
 package com.mkyong.json;
 
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.json.JSONObject;
@@ -44,6 +49,7 @@ public class JsonResource {
         );
     }
 
+    // POST, accepts JSON
     @Path("/create")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
